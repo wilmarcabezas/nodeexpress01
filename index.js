bb2e8f2e944747822202c4ed06a25ab1dd828fe3
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2023-11-23 13:08:27 -0500

    Deploye two

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 app.get('/', (req,res)=>{
-    res.send('Welcome');
+    res.send('Welcome to the world');
 })
 
 app.listen(port,()=>{
